8cb4e7e6c83bf6e1543649e917ac9ea0d871bfe3
VesaKukkonen <vesa.kukkonenlx@gmail.com>
2019-05-02 19:31:15 +0300

    eka commit

---
+++ application/__init__.py
@@ -0,0 +1,28 @@
+# Tuodaan Flask käyttöön
+from flask import Flask
+app = Flask(__name__)
+
+# Tuodaan SQLAlchemy käyttöön
+from flask_sqlalchemy import SQLAlchemy
+# Käytetään task.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
+# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
+# samassa paikassa.
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
+app.config["SQLALCHEMY_ECHO"] = True
+# Korjaa virheen
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+#Luodaan db-olio, jota käytetään tietokannan käsittelyyn
+db = SQLAlchemy(app)
+
+# Luetaan kansiosta application tiedoston views sisältö
+from application import views
+
+# Luetaan kansiosta application/tasks tiedoston models sisältö
+from application.tasks import models
+from application.tasks import views
+
+# Luodaan lopulta tarvittavat tietokantataulut
+db.create_all()
+
